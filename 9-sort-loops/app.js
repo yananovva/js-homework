2937f11b7d72c401f8ab0416b3eb98a9e1aa348b
Yana Uranova <uranovayr@gmail.com>
2024-05-08 11:21:09 +0300

    Add homework sort

---
+++ 9-sort-loops/app.js
@@ -0,0 +1,20 @@
+// Входные данные
+const oldArr = [1, 40, -5, 10, 0];
+
+// Решение
+
+function sorter(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+let newArr = sorter(oldArr);
+console.log(newArr);
