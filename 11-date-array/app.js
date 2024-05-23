4b5c06525d196601ad8492a9f0936845edee9a3a
Yana Uranova <uranovayr@gmail.com>
2024-05-23 13:35:25 +0300

    corrected 11 directory

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -1,56 +1,96 @@
-function processDateStrings(dateStrings) {
-    const validDates = [];
 
-    for (const dateString of dateStrings) {
-        if (isValidDateFormat(dateString)) {
-            const dateComponents = dateString.split(/[-/]/);
-            const day = dateComponents[0].padStart(2, '0');
-            const month = dateComponents[1].padStart(2, '0');
-            const year = dateComponents[2];
+/*
+    Дан массив строк ['10-02-2022','тест','11/12/2023','00/13/2022','41/12/2023'];
+    Необходимо написать функцию, которая удаляла бы из массива все строки, которые нельзя перевести в дату
+    (можно: 10-02-2022 и 11/12/2023) и возвращала новый массив вида:
+    -   ['10-02-2022','12-11-2023']
+*/
 
-            const formattedDate = `${day}.${month}.${year}`;
-            validDates.push(formattedDate);
-        }
+const length = 30;
+
+// Автоматически сгенерированный массив дат по двум типам разделителя "/, -"
+const inputDates = Array.from({ length }, () => {
+    function randomNumber(min = 0, max = 35) {
+        return Math.ceil(Math.random() * (max - min)) + min;
     }
 
-    return validDates;
+    const factor = Math.random() > 0.5 ? '/' : '-';
+
+    const dateArray = [randomNumber(), randomNumber(), randomNumber(1900, 2050)]
+        .map((x) => x.toString().padStart(2, '0'));
+    return dateArray.join(factor);
+});
+
+// Функция получения фильтрации и валидации массива строк в дату по условию, полученному через функцию
+function getDates(array, fn) {
+    return array.map(stringToArray).filter(fn).sort(sortDatePattern).map(formatDate);
 }
 
-function isValidDateFormat(dateString) {
-    const dateComponents = dateString.split(/[-/]/);
+// Функция форматирования результирующей массива даты [day, month, year] в строку
+function formatDate(array) {
+    return array.map((x, i) => (i === 2 ? x.padStart('0', 4) : x.padStart('0', 2))).join('-');
+}
 
-    if (dateComponents.length !== 3) {
+// Функция сортировки даты по возрастанию
+function sortDatePattern(a, b) {
+    let day, month, year;
+
+    [day, month, year] = a;
+    const dt1 = new Date(year, month - 1, day).getTime();
+
+    [day, month, year] = b;
+    const dt2 = new Date(year, month - 1, day).getTime();
+
+    return dt1 - dt2;
+}
+
+// Функция преобразования строки в массив даты [day, month, year] в зависимости локали
+function stringToArray(str) {
+    let day, month, year;
+    if (str.includes('/')) {
+        [month, day, year] = str.split('/');
+    } else if (str.includes('-')) {
+        [day, month, year] = str.split('-');
+    }
+    if (!year || isNaN(day) || isNaN(month) || isNaN(year)) {
+        return null;
+    }
+    return [day, month, year];
+}
+
+// Функция валидации корректности даты
+function checkCorrectDate(array) {
+    if (array === null) {
         return false;
     }
 
-    const day = parseInt(dateComponents[0]);
-    const month = parseInt(dateComponents[1]);
-    const year = parseInt(dateComponents[2]);
+    [day, month, year] = array.map(Number);
 
-    if (isNaN(day) || isNaN(month) || isNaN(year)) {
+    const LONG_MONTH_ARRAY = [1, 3, 5, 7, 8, 10, 12];
+
+    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+    // Проверяем месяц
+    if (month < 1 || month > 12 || year <= 0) {
         return false;
     }
 
-    if (day < 1 || day > 31 || month < 1 || month > 12) {
+    // Проверяем день
+    if (day < 1 || day > 31) {
         return false;
     }
 
-    if (month === 2) {
-        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-            if (day > 29) {
-                return false;
-            }
-        } else {
-            if (day > 28) {
-                return false;
-            }
-        }
+    if (day === 31 && !LONG_MONTH_ARRAY.includes(month)) {
+        return false;
+    }
+
+    // Проверяем февраль и корректность дня в зависимости от високосности года
+    if (month === 2 && day === 29 && !isLeapYear) {
+        return false;
     }
     return true;
 }
 
-
-
-const dates = ['10-02-2022', 'test', '11/12/2023', '00/13/2022', '41/12/2023'];
-const validDates = processDateStrings(dates);
-console.log(validDates);
+const res = getDates(inputDates, checkCorrectDate);
+console.log(inputDates);
+console.log(res);
